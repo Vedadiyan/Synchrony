d09ccb368680615233e7fee3302c4bf1260b9faf
Pouya Vedadiyan <vedadiyan@gmail.com>
2021-08-19 11:50:45 +0430

    Multiple Database Support Fix

--- Synchrony.Test/Program.cs
+++ Synchrony.Test/Program.cs
@@ -10,7 +10,7 @@ namespace Synchrony.Test
     {
         static async Task Main(string[] args)
         {
-            SynchronyDbContext dbContext = new SynchronyDbContext("Data Source=192.168.233.200,7006;Initial Catalog=TestDb;User Id=sa;Password=Pou39148!", 1);
+            SynchronyDbContext dbContext = new SynchronyDbContext("", 1);
             await dbContext.Initialize(new CancellationToken());
             dbContext.Add += (sender, e) =>
             {
@@ -28,7 +28,7 @@ namespace Synchrony.Test
             {
                 await dbContext.GetChanges(new CancellationToken());
                 Console.WriteLine(DateTime.Now);
-            }, null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
+            }, null, TimeSpan.Zero, TimeSpan.FromSeconds(1));
             Console.ReadLine();
         }
     }
