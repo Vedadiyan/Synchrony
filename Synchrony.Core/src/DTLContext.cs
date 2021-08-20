1f4cac94c23f18f788babdcdba28ceef9d542c90
Pouya Vedadiyan <vedadiyan@gmail.com>
2021-08-20 22:53:27 +0430

    Synchronizer

--- Synchrony.Core/src/DTLContext.cs
+++ Synchrony.Core/src/DTLContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Synchrony.Core.Abstraction;
@@ -70,22 +71,64 @@ namespace Synchrony.Core
                 }
             }
         }
+        public async Task<object> Synchronizer(DataRow row, DataColumn synchronizationColumn, CancellationToken cancellationToken)
+        {
+            StringBuilder insertScript = new StringBuilder();
+            StringBuilder _insertScript = new StringBuilder();
+            StringBuilder updateScript = new StringBuilder();
+            insertScript.Append("INSERT INTO ").Append(row.Table.TableName).Append("(");
+            _insertScript.Append("VALUES(");
+            updateScript.Append("UPDATE ").Append(row.Table.TableName).Append(" SET ");
+            for (int i = 0; i < mappers.Count; i++)
+            {
+                Mapper mapper = mappers.ElementAt(i);
+                insertScript.Append(mapper.ToField);
+                updateScript.Append(mapper.ToField).Append("=@").Append(mapper.FromField);
+                _insertScript.Append("@").Append(mapper.FromField);
+                if (i < mappers.Count - 1)
+                {
+                    insertScript.Append(",");
+                    _insertScript.Append(",");
+                    updateScript.Append(",");
+                }
+
+            }
+            insertScript.Append(")");
+            _insertScript.Append(")");
+            updateScript.Append(" WHERE ").Append(synchronizationColumn.ColumnName).Append("=@__$value");
+            using (SqlConnection con = new SqlConnection(destinationConnectionString))
+            {
+                SqlCommand command = con.CreateCommand();
+                command.CommandText = @$"
+                    IF (SELECT TOP 1 1 FROM {row.Table.TableName} WHERE {synchronizationColumn.ColumnName} = @__$value) == 0
+                        {insertScript.ToString()}
+                    ELSE 
+                        {updateScript.ToString()}
+                ";
+                foreach (var i in mappers)
+                {
+                    command.Parameters.AddWithValue($"@{i.FromField}", row.Field<object>(i.FromField));
+                }
+                command.Parameters.AddWithValue("@__$value", row.Field<object>(synchronizationColumn.ColumnName));
+                await con.OpenAsync(cancellationToken);
+                return await command.ExecuteScalarAsync(cancellationToken);
+            }
+        }
         public IMap GetMapper()
         {
             Mapper mapper = new Mapper(this);
             mappers.Add(mapper);
             return mapper;
         }
-
         public void UseOriginalId(string columnName, SqlDbType sqlDbType)
         {
             originalIdColumnName = columnName;
-            originalIdColumnType = sqlDbType; ;
+            originalIdColumnType = sqlDbType;
         }
 
         public DataRow GetInsertedRowFor<T>(T oldId)
         {
-            return insertedData.AsEnumerable().Where(x=> x.Field<T>("__$ref").Equals(oldId)).FirstOrDefault();
+            return insertedData.AsEnumerable().Where(x => x.Field<T>("__$ref").Equals(oldId)).FirstOrDefault();
         }
     }
 }
--- Synchrony.Test/Program.cs
+++ Synchrony.Test/Program.cs
@@ -10,25 +10,7 @@ namespace Synchrony.Test
     {
         static async Task Main(string[] args)
         {
-            SynchronyDbContext dbContext = new SynchronyDbContext("", 1);
-            await dbContext.Initialize(new CancellationToken());
-            dbContext.Add += (sender, e) =>
-            {
-                Console.WriteLine(JsonSerializer.Serialize(e));
-            };
-            dbContext.Delete += (sender, e) =>
-            {
-                Console.WriteLine(JsonSerializer.Serialize(e));
-            };
-            dbContext.Update += (sender, e) =>
-            {
-                Console.WriteLine(JsonSerializer.Serialize(e));
-            };
-            Timer timer = new Timer(async (x) =>
-            {
-                await dbContext.GetChanges(new CancellationToken());
-                Console.WriteLine(DateTime.Now);
-            }, null, TimeSpan.Zero, TimeSpan.FromSeconds(1));
+        
             Console.ReadLine();
         }
     }
